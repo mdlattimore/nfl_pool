8b84c269a17b57d789b74f140f6cf7894a4bdafc
Mark D. Lattimore <mark.d.lattimore@gmail.com>
2025-09-03 08:24:39 -0400

    added 'display_name' property to user model'

--- accounts/models.py
+++ accounts/models.py
@@ -4,8 +4,18 @@ from django.db import models
 class CustomUser(AbstractUser):
     paid = models.BooleanField(default=False)
     notes = models.TextField(blank=True, null=True)
+
+    @property
+    def display_name(self):
+        if self.first_name and self.last_name:
+            return f"{self.first_name} {self.last_name[0]}."
+        return self.username  # fallback if names aren't set
+
     def __str__(self):
         return self.username
 
-    def __str__(self):
-        return self.email
+    # def __str__(self):
+    #     return self.email
+
+
+
